9de99a28fe46e3d86c4b222e15511e72fa4e6464
Eduard Gotwig <gotwig@mail.com>
2016-03-27 19:09:39 +0200

    Fixed healh globe pickup (now actually working full functional), higher moonwell health / mana replenish

--- game/dota_addons/heroesoftheancients/scripts/vscripts/healthglobe.lua
+++ game/dota_addons/heroesoftheancients/scripts/vscripts/healthglobe.lua
@@ -1,7 +1,7 @@
 function dropHealthGlobe(unit)
 		if (unit:GetUnitName() == "npc_dota_creep_badguys_ranged" or unit:GetUnitName() == "npc_dota_creep_goodguys_ranged")
 		then
-			local item = CreateItem("item_flask", nil, nil)
+			local item = CreateItem("item_potion_of_healing", nil, nil)
 			local pos = unit:GetAbsOrigin()
 			local drop = CreateItemOnPositionSync( pos, item )
 			local pos_launch = pos+RandomVector(RandomFloat(150,200))
